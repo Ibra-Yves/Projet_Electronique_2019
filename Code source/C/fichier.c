fe6033d4660c25d32174a5be4e560ff5935ce37a
Ibra-Yves <44869843+Ibra-Yves@users.noreply.github.com>
2019-05-20 16:43:26 +0200

    Update fichier.c

--- Code source/C/fichier.c
+++ Code source/C/fichier.c
@@ -1,14 +1,14 @@
 #include <18F458.h>
 #device ADC=10
 #use delay(crystal=20000000)
-#use rs232(baud=19200,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8)
-#define BCD_UNITS(x) (x % 10) //unité
+#use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8)
+#define BCD_UNITS(x) (x % 10) //unitÃ©
 #define BCD_TENS(x) (x / 10) //dizaine
 
-void afficheUnite(int temperature);
-void afficheDizaine(int temperature);
-void afficheTemp(int temperature);
-void gestLED(int temps, int max, int min);
+void affU(int temp);
+void affD(int temp);
+void affT(int temp);
+void allumeLED(int temp , int max, int min);
 void main()
 {
    setup_adc_ports(AN0);
@@ -16,46 +16,43 @@ void main()
    setup_low_volt_detect(FALSE);
    int max = 30;
    int min = 1;
-   int temperature = 20;
-   int dizaine ;
-   int unite;
-   char typeOFData = "-50";
+   int temp = 20;
    
    set_adc_channel(0);
    while(TRUE)
    {
-  	temperature = read_adc() * 100 / 1024; //mettre en degré
-  	gestLED(temperature,max,min); //relation avec fonction en dessous
-  	afficheTemp(temperature);
+     temp = read_adc() * 100 / 1024; //convertir la tension en tempÃ©rature
+     allumeLED(temp,max,min); //voir fonction plus bas
+     affT(temp);
    }
 }
 
-void gestLED(int temp, int max, int min){
- 	if(temp > max || temp < min){
-  	output_high(PIN_B1); //mise à 1
-  	output_low(PIN_B0); //mise à 0
+void allumeLED(int temp, int max, int min){
+    if(temp > max || temp < min){
+     output_high(PIN_B1); //allume la LED rouge
+     output_low(PIN_B0); //Ã©teint la LED verte 
    }
    else{
-  	output_low(PIN_B1); //mise à 0
-  	output_high(PIN_B0); //mise à 1
+     output_low(PIN_B1); //Ã©teint la LED rouge
+     output_high(PIN_B0); //allume la LED verte
    }
 }
+void affT(int temp){
+     while(TRUE){
+         affU(temp);
+         delay_ms(10);
+         affD(temp);
+         delay_ms(10);
+     }
+}
+void affU(int temp){
+         output_d(BCD_UNITS(temp) | (1 << 4));
+         output_low(PIN_D7);
+         output_high(PIN_D6);
+}
+void affD(int temp){
+         output_d(BCD_TENS(temp) | (1 << 5));
+         output_low(PIN_D6);
+         output_high(PIN_D7);
+}
 
-void afficheTemp(int temperature){
-  	while(TRUE){
-   	   afficheUnite(temperature);
-   	   delay_ms(10);
-   	   afficheDizaine(temperature);
-   	   delay_ms(10);
-  	}
-}
-void afficheUnite(int temperature){
-   	   output_d(BCD_UNITS(temperature) | (1 << 4));
-   	   output_low(PIN_D7);
-   	   output_high(PIN_D6);
-}
-void afficheDizaine(int temperature){
-   	   output_d(BCD_TENS(temperature) | (1 << 5));
-   	   output_low(PIN_D6);
-   	   output_high(PIN_D7);
-}
